6b2afcb1779ff3e01487e1046397bedb5625c52c
Hisashi Takeda <tkdhss111@gmail.com>
2024-10-17 11:14:23 +0900

    change . to % in accordance with Fortran standard.

--- test/unit_test.f90
+++ test/unit_test.f90
@@ -1,7 +1,8 @@
 program unit_test1
   use dt_mo
   implicit none
-  type(dt_ty)                :: dt
+  type(tm_ty)                :: tm ! Timer object
+  type(dt_ty)                :: dt ! Datetime object
   type(dt_ty),   allocatable :: dts(:), dts_hours(:)
   character(19), allocatable :: datetimes(:)
   integer, allocatable       :: poss(:)
@@ -9,9 +10,14 @@ program unit_test1
   character(19)              :: datetime
 
   !
+  ! Timer
+  !
+  call tm%tic()
+
+  !
   ! strftime datetime test
   !
-  dt = dt.now()
+  dt = dt%now()
   print '(a)', 'strftime: %Y-%m-%d 01:00'
   datetime = strftime ( dt, '%Y-%m-%d %H:01:00' )
   print '(a, a)', 'datatime: ', datetime
@@ -21,7 +27,7 @@ program unit_test1
   !
   print '(a)', 'read datatime with T: 2008-01-01T00:00:00'
   dt = strptime ( '2008-01-01T00:00:00' )
-  print '(a, a)', 'datatime with T: ', dt.datetime
+  print '(a, a)', 'datatime with T: ', dt%datetime
 
   !
   ! dt_ty object 
@@ -33,36 +39,36 @@ program unit_test1
 
   ! Test current time
   print '(a)', "Current time"
-  dt = dt.now ()
-  print '(a40, a30)', "dt.now (): ", dt.datetime 
+  dt = dt%now ()
+  print '(a40, a30)', "dt%now (): ", dt%datetime 
   print '(a)', ''
 
   ! Test user-defined time
   print '(a)', "User-defined time"
   dt = strptime ( '2020-12-31 23:04:05' )
-  print '(a40, a30)', "strptime ( '2020-12-31 23:04:05' ): ", dt.datetime 
+  print '(a40, a30)', "strptime ( '2020-12-31 23:04:05' ): ", dt%datetime 
   print '(a)', ''
 
   ! Test user-defined time with format
   print '(a)', "User-defined time with format"
   dt = strptime ( '2020-12-31 23:04:05' )
   dt = strptime ( '201231', fmt0 = '%y%m%d' )
-  print '(a40, a30)', "strptime ( '201231', fmt0 = '%y%m%d' ): ", dt.datetime 
+  print '(a40, a30)', "strptime ( '201231', fmt0 = '%y%m%d' ): ", dt%datetime 
   print '(a)', ''
 
   ! Test dt_ty object via character array
-  print '(a)', "dt_ty obset setup via charactger array"
+  print '(a)', "dt_ty object setup via charactger array"
   datetimes = [ '2020-01-03 03:04:05', '2001-02-03 04:05:06' ]
   dts = strptime ( datetimes )
   n = size(dts)
-  print '(*(1x, a19))',       "datetime : ", [(dts(i).datetime,  i = 1, n)]
+  print '(*(1x, a19))',       "datetime : ", [(dts(i)%datetime,  i = 1, n)]
   print '(a)', ''
 
   ! Test dt_ty object via integer array ( get_datetime function test )
-  print '(a)', "dt_ty obset setup via integer array ( get_datetime function test )"
+  print '(a)', "dt_ty object setup via integer array ( get_datetime function test )"
   datetimes = get_datetime ( yr0=[2020, 2012, 2000], mo0=[2,3,4], dy0=[1,4,6], hr0=[4,0,5],mi0=[1,3,5], sc0=[0,20,30] ) 
   dts = strptime ( datetimes )
-  print '(*(1x, a19))',       "datetime : ", [(dts(i).datetime,  i = 1, n)]
+  print '(*(1x, a19))',       "datetime : ", [(dts(i)%datetime,  i = 1, n)]
   print '(a)', ''
 
   dts = seq_dt ( strptime ( '2020-12-31 23:04:05' ), strptime ( '2021-01-02 23:04:05' ), by = '1 day' )
@@ -76,31 +82,31 @@ program unit_test1
   print '(a)', repeat('-', 79)
   print '(a)', "String members of dt_ty"
   print '(a)', repeat('-', 79)
-  print '(*(1x, a19))',       "date     : ", [(dts(i).date,  i = 1, n)]
-  print '(*(1x, a19))',       "time     : ", [(dts(i).time,  i = 1, n)]
-  print '(*(1x, a19))',       "yyyy     : ", [(dts(i).yyyy,  i = 1, n)]
-  print '(*(1x, a19))',       "yy       : ", [(dts(i).yy,    i = 1, n)]
-  print '(*(1x, a19))',       "mm       : ", [(dts(i).mm,    i = 1, n)]
-  print '(*(1x, a19))',       "dd       : ", [(dts(i).dd,    i = 1, n)]
-  print '(*(1x, a19))',       "hh       : ", [(dts(i).hh,    i = 1, n)]
-  print '(*(1x, a19))',       "nn       : ", [(dts(i).nn,    i = 1, n)]
-  print '(*(1x, a19))',       "ss       : ", [(dts(i).ss,    i = 1, n)]
-  print '(1x, a19, *(1x, a19))', "day of the week : ", [(LAB_WDAY(dts(i).dow),  i = 1, n)]
+  print '(*(1x, a19))',       "date     : ", [(dts(i)%date,  i = 1, n)]
+  print '(*(1x, a19))',       "time     : ", [(dts(i)%time,  i = 1, n)]
+  print '(*(1x, a19))',       "yyyy     : ", [(dts(i)%yyyy,  i = 1, n)]
+  print '(*(1x, a19))',       "yy       : ", [(dts(i)%yy,    i = 1, n)]
+  print '(*(1x, a19))',       "mm       : ", [(dts(i)%mm,    i = 1, n)]
+  print '(*(1x, a19))',       "dd       : ", [(dts(i)%dd,    i = 1, n)]
+  print '(*(1x, a19))',       "hh       : ", [(dts(i)%hh,    i = 1, n)]
+  print '(*(1x, a19))',       "nn       : ", [(dts(i)%nn,    i = 1, n)]
+  print '(*(1x, a19))',       "ss       : ", [(dts(i)%ss,    i = 1, n)]
+  print '(1x, a19, *(1x, a19))', "day of the week : ", [(LAB_WDAY(dts(i)%dow),  i = 1, n)]
 
   print '(a)', ''
 
   print '(a)', repeat('-', 79)
   print '(a)',  "Integer members of dt_ty"
   print '(a)', repeat('-', 79)
-  print '(1x, a19, *(1x, i19))', "yr      : ", [(dts(i).yr,   i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "mo      : ", [(dts(i).mo,   i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "dy      : ", [(dts(i).dy,   i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "hr      : ", [(dts(i).hr,   i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "mi      : ", [(dts(i).mi,   i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "sc      : ", [(dts(i).sc,   i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "leap    : ", [(dts(i).leap, i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "doy     : ", [(dts(i).doy,  i = 1, n)]
-  print '(1x, a19, *(1x, i19))', "dow     : ", [(dts(i).dow,  i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "yr      : ", [(dts(i)%yr,   i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "mo      : ", [(dts(i)%mo,   i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "dy      : ", [(dts(i)%dy,   i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "hr      : ", [(dts(i)%hr,   i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "mi      : ", [(dts(i)%mi,   i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "sc      : ", [(dts(i)%sc,   i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "leap    : ", [(dts(i)%leap, i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "doy     : ", [(dts(i)%doy,  i = 1, n)]
+  print '(1x, a19, *(1x, i19))', "dow     : ", [(dts(i)%dow,  i = 1, n)]
   print '(a)', ''
 
   !
@@ -121,10 +127,10 @@ program unit_test1
   print '(1x, a50, a30)', "strftime ( dt, '%H 時 %m/%d, %Y' ): ",            strftime ( dt, '%H 時 %m/%d, %Y' )
 
   dt = strptime ( '2010-01-01 02:04', '%Y-%m-%d %H:%M' )
-  print '(a)', 'strptime ( "2010-01-01 02:04", "%Y-%m-%d %H:%M" ): ', dt.datetime
+  print '(a)', 'strptime ( "2010-01-01 02:04", "%Y-%m-%d %H:%M" ): ', dt%datetime
 
   dt = strptime ( '04:02 2010/01/01', '%H:%M %Y/%m/%d' )
-  print '(a)', 'strptime ( "04:02 2010/01/01", "%H:%M %Y/%m/%d" ): ', dt.datetime
+  print '(a)', 'strptime ( "04:02 2010/01/01", "%H:%M %Y/%m/%d" ): ', dt%datetime
   print '(a)', ''
 
   !
@@ -135,29 +141,29 @@ program unit_test1
   print '(a)', 'Check         : time addition and subtraction'
   print '(a)', repeat('=', 79)
 
-  print *, "Before addition dt.datetime: ", dt.datetime 
-  dt = dt.plus ( days = 5 )
-  print '(a40, a30)', "dt.plus ( days = 5 ): ", dt.datetime 
+  print *, "Before addition dt%datetime: ", dt%datetime 
+  dt = dt%plus ( days = 5 )
+  print '(a40, a30)', "dt%plus ( days = 5 ): ", dt%datetime 
 
-  dt = dt.plus ( hrs = 5 )
-  print '(a40, a30)', "dt.plus ( hrs = 5 ): ", dt.datetime 
+  dt = dt%plus ( hrs = 5 )
+  print '(a40, a30)', "dt%plus ( hrs = 5 ): ", dt%datetime 
 
-  dt = dt.plus ( mins = 5 )
-  print '(a40, a30)', "dt.plus ( mins = 5 ): ", dt.datetime 
+  dt = dt%plus ( mins = 5 )
+  print '(a40, a30)', "dt%plus ( mins = 5 ): ", dt%datetime 
 
-  dt = dt.plus ( secs = 5 )
-  print '(a40, a30)', "dt.plus ( secs = 5 ): ", dt.datetime 
+  dt = dt%plus ( secs = 5 )
+  print '(a40, a30)', "dt%plus ( secs = 5 ): ", dt%datetime 
 
-  dt = dt.plus ( days = 4, hrs = 3, mins = 2, secs = 1 )
-  print '(a50, a20)', "dt.plus ( days = 4, hrs = 3, mins = 2, secs = 1 ): ", dt.datetime 
+  dt = dt%plus ( days = 4, hrs = 3, mins = 2, secs = 1 )
+  print '(a50, a20)', "dt%plus ( days = 4, hrs = 3, mins = 2, secs = 1 ): ", dt%datetime 
 
-  dt = dt.minus ( days = 15 )
-  print '(a40, a30)', "dt.minus ( days = 15 ): ", dt.datetime 
+  dt = dt%minus ( days = 15 )
+  print '(a40, a30)', "dt%minus ( days = 15 ): ", dt%datetime 
 
-  dt = dt.minus ( hrs = 5 )
-  print '(a40, a30)', "dt.minus ( hrs = 5 ): ", dt.datetime 
+  dt = dt%minus ( hrs = 5 )
+  print '(a40, a30)', "dt%minus ( hrs = 5 ): ", dt%datetime 
 
-  dt = dt.minus ( mins = 5 )
+  dt = dt%minus ( mins = 5 )
   print '(a40, a30)', "dt%minus ( mins = 5 ): ", dt%datetime 
 
   dt = dt%minus ( secs = 5 )
@@ -212,7 +218,7 @@ program unit_test1
   poss = get_pos_rec ( ['2008-01-02 00:00:00', '2008-01-03 00:00:00'] )
   print '(a, i0, 1x, i0)', 'Writing position by 30 secs from the epoch: ', poss
 
-  open ( newunit = u, file = 'test.csv', form = 'unformatted', access = 'direct', recl = 5 )
+  open ( newunit = u, file = 'test%csv', form = 'unformatted', access = 'direct', recl = 5 )
 
   write ( u, rec = get_pos_rec ( '2008-01-01 00:00:00' ) ) 'test1'
   write ( u, rec = get_pos_rec ( '2008-01-01 00:30:00' ) ) 'test2'
@@ -225,6 +231,8 @@ program unit_test1
   ! On spot test
   dts = seq_dt ( strptime ( '2008-06-25 00:00:00' ), strptime ( '2024-09-24 00:00:00' ), by = '1 day' )
   print *, 'size(dts): ', size(dts), ', dts(size(dts))%datetime: ', dts(size(dts))%datetime
+
+  call tm%toc()
 
 contains
 
@@ -244,7 +252,7 @@ contains
     print '(a)', repeat('-', 79)
 
     print '(1x, a, i0, a, i0, a, i0, a, i0, a)', &
-    'Time difference: ', n%days, 'days, ', n%hrs, 'hrs, ', n%mins, 'mins, ', n%secs, 'secs.'
+    'Time difference: ', n%days, 'days, ', n%hrs, 'hrs, ', n%mins, 'mins, ', n%secs, 'secs%'
 
     nsecs = dts0(2) - dts0(1)
     print '(1x, a, i0)', "nsecs: ", nsecs
